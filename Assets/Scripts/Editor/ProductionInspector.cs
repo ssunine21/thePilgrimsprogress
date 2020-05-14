d9731ad3856f95c6a41305d5d86d45e2a106effc
ssunine21 <xcvbnm757@naver.com>
2020-05-14 17:00:38 +0900

    #0514

---
+++ Assets/Scripts/Editor/ProductionInspector.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using UnityEditorInternal;
+
+
+[CustomEditor(typeof(ProductionManager))]
+public class ProductionInspector : Editor {
+    private ReorderableList list;
+
+    ProductionManager productionManager;
+
+    private void OnEnable() {
+        productionManager = target as ProductionManager;
+        list = new ReorderableList(serializedObject, serializedObject.FindProperty("productionList"), true, true, true, true);
+
+        list.drawElementCallback = (Rect rect, int index, bool isActive, bool isFocused) => {
+            var element = list.serializedProperty.GetArrayElementAtIndex(index);
+            rect.y += 2;
+            EditorGUI.PropertyField(
+                new Rect(rect.x, rect.y, 60, EditorGUIUtility.singleLineHeight), element.FindPropertyRelative("pos"), GUIContent.none);
+            EditorGUI.PropertyField(
+                new Rect(rect.x + 60, rect.y, rect.width - 60 - 30, EditorGUIUtility.singleLineHeight),
+                element.FindPropertyRelative("moveSpeed"), GUIContent.none);
+            EditorGUI.PropertyField(
+                new Rect(rect.x + rect.width - 30, rect.y, 30, EditorGUIUtility.singleLineHeight),
+                element.FindPropertyRelative("delayTime"), GUIContent.none);
+            EditorGUI.PropertyField(
+                new Rect(rect.x + rect.width - 30, rect.y, 30, EditorGUIUtility.singleLineHeight),
+                element.FindPropertyRelative("anim"), GUIContent.none);
+        };
+
+    }
+
+    // 유니티가 인스펙터를 GUI로 그려주는함수
+    public override void OnInspectorGUI() {
+
+        productionManager.production = (Production)EditorGUILayout.EnumPopup("Type", productionManager.production);
+
+        serializedObject.Update();
+        list.DoLayoutList();
+
+        serializedObject.ApplyModifiedProperties();
+    }
+}
--- Assets/Scripts/ProductionManager.cs
+++ Assets/Scripts/ProductionManager.cs
@@ -1,19 +1,8 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEditor;
 
-
-
-[System.Serializable]
-public struct CharacterProductionList {
-    public Vector2 currPos;
-    public float moveSpeed;
-    public Vector2 prevPos;
-    public Animation animation;
-    public float delayTime;
-}
-
-[System.Serializable]
 public enum Production // your custom enumeration
 {
     Character,
@@ -21,9 +10,37 @@ public enum Production // your custom enumeration
     Object
 };
 
+[System.Serializable]
+public class ProductionList {
+    public Vector2 pos;
+    public float moveSpeed;
+    public float delayTime;
+    public Animation anim;
+}
+
 public class ProductionManager : MonoBehaviour
 {
-    public Production production;
-    public ArrayList prodectionList;
 
+    public Production production;
+    public List<ProductionList> productionList = new List<ProductionList>();
+
+    ProductionManager() {
+        productionList[0].pos = this.transform.position;
+    }
+
+    void OnDrawGizmosSelected() {
+        Gizmos.color = Color.red;
+        Gizmos.DrawCube(transform.position, Vector2.one);
+
+        for (int i = 0; i < productionList.Count; ++i) {
+            if (i == 0) productionList[0].pos = this.transform.position;
+
+            if (productionList.Count - 1 == i) {
+                productionList[i].pos = Handles.PositionHandle(productionList[i].pos, Quaternion.identity);
+                break;
+            } else
+                Handles.DrawLine(productionList[i].pos, productionList[i + 1].pos);
+        }
+
+    }
 }
